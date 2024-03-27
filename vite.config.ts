e2c1aa3d91d410de0cbbf7550aa43f7275a52007
Mhd Qddo <muhammadqddo@gmail.com>
2024-03-27 23:18:22 +0300

    Edit vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://mhdqddo.github.io/con3x.task001.webpage/",
+  base: "https://github.com/MhdQddo/con3x.task001.webpage/tree/main/",
 })
